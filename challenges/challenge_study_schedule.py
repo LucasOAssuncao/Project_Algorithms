d17f3df4101ca976d40ddaab3a073230ffe6369c
Lucas Otoni <lotoni.vivo@gmail.com>
2023-02-16 15:44:48 -0300

    feat: study-schedule implementada

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+
+    student_count = 0
+    for start, out in permanence_period:
+        if not isinstance(start, int) or not isinstance(out, int):
+            return None
+        if start <= target_time <= out:
+            student_count += 1
+    return student_count
