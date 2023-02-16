3cf23e409fa15f6cbfc8f61f866a3a415767f655
Lucas Otoni <lotoni.vivo@gmail.com>
2023-02-16 16:13:50 -0300

    feat: test_encrypt implementada

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
-
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 4)
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("coca", "oi")
+    assert encrypt_message("coca", 100) == "acoc"
+
+    assert encrypt_message("coca", 3) == "coc_a"
+    assert encrypt_message("coca", 2) == "ac_oc"
+
+
