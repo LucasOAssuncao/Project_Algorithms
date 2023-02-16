56967120116553e8df2588baf68e1387ac76964b
Lucas Otoni <lotoni.vivo@gmail.com>
2023-02-16 16:17:55 -0300

    fix: arrumando problema de lint

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,6 +1,7 @@
 import pytest
 from challenges.challenge_encrypt_message import encrypt_message
 
+
 def test_encrypt_message():
     with pytest.raises(TypeError, match="tipo inválido para message"):
         encrypt_message(1, 4)
@@ -10,5 +11,3 @@ def test_encrypt_message():
 
     assert encrypt_message("coca", 3) == "coc_a"
     assert encrypt_message("coca", 2) == "ac_oc"
-
-
